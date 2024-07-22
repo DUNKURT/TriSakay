e540a6e22c5beb1d3eb536021e37c52aa997bcc9
Dan <123217422+DanSSV@users.noreply.github.com>
2024-07-22 14:24:00 +0800

    Authpages

---
+++ authPages/authPage.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TriSakay</title>
+    <link rel="stylesheet" href="../css/authPage.css">
+    <?php include '../views/cdn/cdn.php'; ?>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <main>
+        <div class="display">
+            <p class="logo">TriSakay</p>
+            <p class="slogan">Your local ride, just a tap away</p>
+        </div>
+
+        <div class="container">
+            <div class="vector">
+                <button class="login"></button>
+                <button class="signup"></button>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
---
+++ views/cdn/cdn.php
@@ -0,0 +1,6 @@
+<?php
+echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">';
+echo '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>';
+echo '<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />';
+echo '<script src="https://kit.fontawesome.com/965a209c77.js" crossorigin="anonymous"></script>';
+?>
